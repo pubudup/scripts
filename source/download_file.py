769201f5e3f9a6113b9e81c42ff6a275e6feec8b
pubudup <pubudu.priyashan@gmail.com>
2025-06-16 23:03:54 +0100

    remove unwanted file

--- source/download_file.py
+++
@@ -1,8 +0,0 @@
-#downloads a PDF file into local directory
-import requests
-
-url ="https://download.microsoft.com/download/d/0/0/d0001082-eedc-41e2-a7c2-4bd05a7fb63a/BusinessApps_ReleaseNotes_v18.1.4.pdf"
-response = requests.get(url)
-
-with open("BusinessApps_ReleaseNotes_v18.1.4.pdf", "wb") as f:
-    f.write(response.content)
